a39a7e1e007842a4dac6d355e1e138d3c57ab796
Renato <renatovalerio25@gmail.com>
2017-05-16 15:30:55 -0300

    test Flex

--- app2/index.android.js
+++ app2/index.android.js
@@ -1,23 +1,42 @@
 import React from 'react';
-import { Text, AppRegistry } from 'react-native';
+import { Text, AppRegistry,View } from 'react-native';
 //estilos
 
 const Styles = {
   styleText: {
     fontSize: 30,
     backgroundColor: '#08509B',
-    padding: 10,
-    margin: 30
-   
+    // padding: 10,
+    // margin: 50
+    color: '#fff',
+    //textDecorationLine: 'underline'
+    textAlign: 'center',
+    heigth: 60,
+    width: 60
+    //fontStyle: 'italic',
+    //fontWeight: 'bold'//100 e 900
+  },
+  styleView: {
+    backgroundColor: 'skyblue',
+    heigth: 300,
+    justifyContent: 'center',//alinha na vertical
+    alignItems: 'flex-end'//alinha na horizontal
+    //flexDorection: 'row' --define a direção que as caixas ficaram;
+
   }
 };
 
 //componentes
 const App = () => {
-  return (
-    <Text style={Styles.styleText}>
-      Frases do dia
-    </Text>
+ 
+  const {styleText, styleView} = Styles;
+   
+    return (
+  <View style= {styleView}>
+     <Text style={styleText}>
+       Frases do dia
+        </Text>
+          </View>
   );
 
 };
