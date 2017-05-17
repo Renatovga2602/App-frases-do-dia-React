d23d30fdca80fc4b7941b41eb8047fa7337c0db0
Renato <renatovalerio25@gmail.com>
2017-05-16 21:10:22 -0300

    icon App and Phrase generator

--- app2/index.android.js
+++ app2/index.android.js
@@ -1,58 +1,62 @@
 import React from 'react';
-import { Text, AppRegistry,View,Button,TouchableOpacity } from 'react-native';
+import { Text, AppRegistry, View, Image, TouchableOpacity,Alert } from 'react-native';
 //estilos
 
 const Styles = {
   principal: {
- 
-    paddingTop: 40
+    paddingTop: 40,
+    // backgroundColor: '#538530',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
- 
-  botao:{
-  
-    backgroundColor: '#48BBEC',
-    padding: 10,
-    borderColor: '#d8eb8',
-    borderWidth: 1,
-    borderRadius: 10,
-    shadowColor: '#000',
-    shadowOffset: {width:0, heigth:2},
-    shadowOpacity: 0.4
-},
-  textoBotao:{
-   
+  botao: {
+    backgroundColor: '#538530',
+    paddingVertical: 10,
+    paddingHorizontal: 40,
+    marginTop: 50
+  },
+  textoBotao: {
     color: '#fff',
     fontSize: 16,
-    fontWeight: 'bold',
-    alignSelf: 'center'
+    fontWeight: 'bold'
   }
-
 };
-const buttonPress = () => {
-  alert ('Butão pressionado');
+var gerarNovaFrase = () => {
+  var numeroAleatorio = Math.random();
+  
+  numeroAleatorio = Math.floor(numeroAleatorio * 7);
+  
+  var frases= Array();
+  frases[0] = 'Amo a Hanny o dia todo !';
+  frases[1] = 'Amo a Hanny toda Hora !';
+  frases[2] = 'Amo você minha princesa !'; 
+  frases[3] = 'Amo seu pé !'; 
+  frases[4] = 'Amo seu chulé, mentira kk !';
+  frases[5] = 'Amo sair para sair com voce meu amor !';
+  frases[6] = 'Amo você, mais amo mais seu pé !';
+
+  var fraseEscolhida = frases[numeroAleatorio];
+
+  Alert.alert(fraseEscolhida);
+   
 }
+
 //componentes
 const App = () => {
- 
-  const {principal, botao,textoBotao} = Styles;
-   
-    return (
-      <View style={principal}>
-        
 
-        <Button
-          onPress={buttonPress}
-          title= 'Clique aqui'
-          color= '#841584'
-          accessibilityLabel='Clique para abrir as noticias!!!'//recurso de acessibilidade
-        
-        />
-        <TouchableOpacity style={botao}>
-          <Text style={textoBotao}>Clique aqui!</Text>
-        </TouchableOpacity>
+  const { principal, botao, textoBotao } = Styles;
 
-      </View>
-  
+  return (
+    <View style={principal}>
+      <Image source={require('./imgs/logo.png')} />
+      <TouchableOpacity
+        onPress={gerarNovaFrase}
+        style={botao}>
+        <Text style={textoBotao}>Nova Frase !</Text>
+      </TouchableOpacity>
+    </View>
+
   );
 
 };
